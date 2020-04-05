6d882e0e4cf4a64e313e302cd9b7683f1e6457bc
Voinduha <voinduha@list.ru>
2020-04-05 16:37:05 +0800

    Создание остальных функций

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -8,6 +8,9 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    EditText el1 = findViewById(R.id.num1);
+    EditText el2 = findViewById(R.id.num2);
+    TextView resText = findViewById(R.id.result);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,9 +19,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onClickPlusBtn ( View v) {
-        EditText el1 = findViewById(R.id.num1);
-        EditText el2 = findViewById(R.id.num2);
-        TextView resText = findViewById(R.id.result);
 
         int num1 = Integer.parseInt(el1.getText().toString());
         int num2 = Integer.parseInt(el2.getText().toString());
@@ -26,4 +26,31 @@ public class MainActivity extends AppCompatActivity {
         resText.setText(Integer.toString(resSum));
 
     }
+    public void onClickMinusBtn ( View v) {
+
+        int num1 = Integer.parseInt(el1.getText().toString());
+        int num2 = Integer.parseInt(el2.getText().toString());
+        int resSum = num1 - num2;
+        resText.setText(Integer.toString(resSum));
+
+    }
+
+    public void onClickMultiplyBtn ( View v) {
+
+        int num1 = Integer.parseInt(el1.getText().toString());
+        int num2 = Integer.parseInt(el2.getText().toString());
+        int resSum = num1 * num2;
+        resText.setText(Integer.toString(resSum));
+
+    }
+
+    public void onClickDelBtn ( View v) {
+
+        int num1 = Integer.parseInt(el1.getText().toString());
+        int num2 = Integer.parseInt(el2.getText().toString());
+        int resSum = num1 / num2;
+        resText.setText(Integer.toString(resSum));
+
+    }
+
 }
