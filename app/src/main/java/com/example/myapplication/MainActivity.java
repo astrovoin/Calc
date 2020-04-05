1a955a45a306af460e264f2d283b54b6472b4caf
Voinduha <voinduha@list.ru>
2020-04-05 16:07:34 +0800

    Создание плюсования

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickPlusBtn ( View v) {
+        EditText el1 = findViewById(R.id.num1);
+        EditText el2 = findViewById(R.id.num2);
+        TextView resText = findViewById(R.id.result);
+
+        int num1 = Integer.parseInt(el1.getText().toString());
+        int num2 = Integer.parseInt(el2.getText().toString());
+        int resSum = num1 + num2;
+        resText.setText(Integer.toString(resSum));
+
     }
 }
